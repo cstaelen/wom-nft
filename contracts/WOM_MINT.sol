3fb079a6881aae6f8dddcd55e943e8b3f65e7d80
Remix Workspace <>
2022-10-27 17:28:58 +0200

    Custom mint amount

--- contracts/WOM_MINT.sol
+++ contracts/WOM_MINT.sol
@@ -4,16 +4,15 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/PullPayment.sol";
 
 contract WOM is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
   string public baseURI;
   string public baseExtension = ".json";
-  uint256 public cost = 0 ether; // @TODO remove ?
   uint256 public maxSupply = 13; // @TODO set to 1000
-  uint256 public nftPerAddressLimit = 4;
+  uint256 public maxMintAmount = 2;
+  uint256 public nftPerAddressLimit = 2;
   bool public paused = false;
   bool public onlyWhitelisted = true;
   address[] public whitelistedAddresses;
@@ -33,26 +32,23 @@ contract WOM is ERC721Enumerable, Ownable {
   }
 
   // public
-  function mint() public payable {
+  function mint(uint256 _mintAmount) public payable {
     require(!paused, "the contract is paused");
     uint256 supply = totalSupply();
-    uint256 mintAmount = 1;
-    require(supply + mintAmount <= maxSupply, "max NFT limit exceeded");
+    require(_mintAmount > 0, "need to mint at least 1 NFT");
+    require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
 
     if (msg.sender != owner()) {
-        if(onlyWhitelisted == true) {
-            mintAmount = 2;
-            require(isWhitelisted(msg.sender), "user is not whitelisted");
-            uint256 ownerMintedCount = addressMintedBalance[msg.sender];
-            require(ownerMintedCount + mintAmount <= nftPerAddressLimit, "max NFT per address exceeded");
-        }
-        require(msg.value >= cost * mintAmount, "insufficient funds");
-    } else {
-      // For the owner
-      mintAmount = 2;
+      require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
+
+      if(onlyWhitelisted == true) {
+          require(isWhitelisted(msg.sender), "user is not whitelisted");
+          uint256 ownerMintedCount = addressMintedBalance[msg.sender];
+          require(ownerMintedCount + _mintAmount <= nftPerAddressLimit, "max NFT per address exceeded");
+      }
     }
 
-    for (uint256 i = 1; i <= mintAmount; i++) {
+    for (uint256 i = 1; i <= _mintAmount; i++) {
       addressMintedBalance[msg.sender]++;
       _safeMint(msg.sender, supply + i);
     }
@@ -60,7 +56,7 @@ contract WOM is ERC721Enumerable, Ownable {
 
   function contractURI() public pure returns (string memory) {
     // @TODO Update with true data
-    return "https://bafkreihmsuwqlpsqx6akglirc5e2ozx7uhbjxkaudqm3v3f3o4nib57msi.ipfs.nftstorage.link/";
+    return "https://bafkreickzcdxcbygczoczyrkzzii5hddgwd4k2vk5wgamm3pk325k7thny.ipfs.nftstorage.link/";
   }
   
   function isWhitelisted(address _user) public view returns (bool) {
@@ -105,10 +101,6 @@ contract WOM is ERC721Enumerable, Ownable {
   
   function setNftPerAddressLimit(uint256 _limit) public onlyOwner {
     nftPerAddressLimit = _limit;
-  }
-  
-  function setCost(uint256 _newCost) public onlyOwner {
-    cost = _newCost;
   }
 
   function setBaseURI(string memory _newBaseURI) public onlyOwner {
